0db86b1913baa3aa34984ba6a25bd0893728644f
LeoLau <liuyuansix@163.com>
2019-07-01 11:28:48 +0800

    new

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <div class="btn-group">
+      <button type="button" class="btn btn-default">left</button>
+      <button type="button" class="btn btn-default">center</button>
+      <button type="button" class="btn btn-default">right</button>
+    </div>
+    <component-a></component-a>
+  </div>
+</template>
+<script>
+
+var ComponentA = {
+  data: function (){
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="greet">You clicked me {{ count }} times.</button>',
+  methods: {
+    greet: function () {
+      console.log('click ')
+      $(function () {
+        alert('123')
+      })
+    }
+  }
+}
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    'component-a': ComponentA
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+        <div class="modal-dialog" style="margin-top: 10%;width: 300px;">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title text-center" id="myModalLabel">登录</h4>
+            </div>
+            <div class="modal-body" id = "model-body">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="用户名" autocomplete="off" v-model="user">
+                </div>
+                <div class="form-group"  style="margin-bottom:0px">
+                    <input type="password" class="form-control" placeholder="密码" autocomplete="off" v-model="password">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <div class="form-group">
+                    <button type="button" class="btn btn-primary form-control" @click="login">登录</button>
+                </div>
+                <div class="form-group" style="margin-bottom:0px">
+                    <button type="button" class="btn btn-default form-control">注册</button>
+                </div>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal -->
+    </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    login: function () {
+      console.log('登录了 ' + this.user + ' ' + '密码 ' + this.password)
+    }
+  },
+  data () {
+    return {
+      user: '',
+      password: '',
+      message: '大宇车险管理系统'
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
