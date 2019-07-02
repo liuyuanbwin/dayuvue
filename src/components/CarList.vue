b75463a3cc0ca1e87855852ae91191019b03dc9b
LeoLau <liuyuansix@163.com>
2019-07-02 18:41:04 +0800

    sk

--- src/components/CarList.vue
+++ src/components/CarList.vue
@@ -1,67 +1,130 @@
 <template>
-     <div>
-         <div class="panel panel-primary" v-for="(car, index) in cars" :key="index">
-            <div class="panel-heading"  >
-                <h1 class="text-left panel-title">{{car.number}}</h1>
+    <div>
+        <div class="panel panel-primary" v-for="(car, index) in cars" :key="index">
+            <div class="panel-heading">
+                <div class="carplate">
+                    {{car.number}}
+                </div>
+                <div style="margin-left:280px;text-align:left;height:30px;">{{car.brand}}</div>
+                <div style="margin-left:280px;text-align:left;height:30px;">{{car.model}}</div>
+                <h4><a data-toggle="collapse" href="#collapse1" class="expand">展开</a></h4>
             </div>
-             <table class="table" id="carinfo" >
-   <thead>
-      <tr>
-         <th class="text-center">项目</th>
-         <th class="text-center">名称</th>
-         <th class="text-center">详情</th>
-      </tr>
-   </thead>
-   <tbody>
-      <tr>
-         <td>车型</td>
-         <td>{{car.brand}}</td>
-         <td>{{car.model}}</td>
-      </tr>
-      <tr>
-         <td>投保人</td>
-         <td>{{car.policyholder}}</td>
-         <td>{{car.policyholderTel}}</td>
-      </tr>
-      <tr>
-         <td>被保险人</td>
-         <td>{{car.insurant}}</td>
-         <td>{{car.insurantTel}}</td>
-      </tr>
-      <tr>
-         <td>联系人</td>
-         <td>{{car.linkman}}</td>
-         <td>{{car.linkmanTel}}</td>
-      </tr>
-   </tbody>
-</table>
-           
+            <div id="collapse1" class="panel-collapse collapse">
+                <div class="panel-body" style="padding-bottom:0px;">
+                    <table class="table" id="carinfo" style="margin-bottom:0px;">
+                        <thead>
+                            <tr>
+                                <th class="text-center">身份</th>
+                                <th class="text-center">姓名</th>
+                                <th class="text-center">电话</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>投保人</td>
+                                <td>{{car.policyholder}}</td>
+                                <td>{{car.policyholderTel}}</td>
+                            </tr>
+                            <tr>
+                                <td>被保险人</td>
+                                <td>{{car.insurant}}</td>
+                                <td>{{car.insurantTel}}</td>
+                            </tr>
+                            <tr>
+                                <td>联系人</td>
+                                <td>{{car.linkman}}</td>
+                                <td>{{car.linkmanTel}}</td>
+                            </tr>
+                        </tbody>
+                        <thead>
+                            <tr>
+                                <th class="text-center">项目</th>
+                                <th class="text-center">时间</th>
+                                <th class="text-center">操作</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>商业险</td>
+                                <td>{{car.policyholder}}</td>
+                                <td>{{car.policyholderTel}}</td>
+                            </tr>
+                            <tr>
+                                <td>交强险</td>
+                                <td>{{car.insurant}}</td>
+                                <td>{{car.insurantTel}}</td>
+                            </tr>
+                            <tr>
+                                <td>日期</td>
+                                <td>{{car.linkman}}</td>
+                                <td>{{car.linkmanTel}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="panel-heading">
+                    <h4 class="panel-title">
+                        <a data-toggle="collapse" href="#collapse2">历史记录</a>
+                    </h4>
+                </div>
+                <div id="collapse2" class="panel-collapse collapse sideborder">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            购商险
+                        </li>
+                        <li class="list-group-item">购交强险</li>
+                        <li class="list-group-item">验车</li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- <form role="form">
+                <div class="form-group panel-body">
+                    <label for="name">文本框</label>
+                    <textarea class="form-control" rows="3"></textarea>
+                </div>
+            </form> -->
         </div>
     </div>
 </template>
 
 <script>
-export default {
-    name: 'CarList',
-    data(){
-        return {
-            cars: []
-        }
-    },
-    created() {
-        this
+    export default {
+        name: 'CarList',
+        data() {
+            return {cars: []}
+        },
+        created() {
+            this
                 .$http
                 .get("http://localhost:3000/cars")
                 .then((data) => {
                     console.log(data)
                     this.cars = data.body
                 })
+        }
     }
-}
 </script>
 
 <style>
-.fullwidth{
-    width: 100%
-}
+    .fullwidth {
+        width: 100%;
+    }
+    .carplate{
+        background-color: #1260EC;
+        color:white;
+        border: 2px solid white;
+        height: 60px;
+        width:268px;
+        font-size: 46px;
+        display: inline-block;
+        float: left;
+    }
+    .expand:link,.expand:visited{
+        color: #ffffff
+    }
+    .sideborder{
+        margin-right:10px;
+        margin-left:10px;
+    }
 </style>
--- src/components/Manage.vue
+++ src/components/Manage.vue
@@ -1,98 +1,102 @@
 <template>
- <div class="">
-    <div class="row" style="margin:0px 10px 10px 0px;">
-        <div class="col-sm-2 col-md-2 col-lg-2" style="padding:0px 10px 0px 0px;margin:0px 20px 0px 0px;z-index:9999;position:fixed;top:70px;left:15px;" >
-            <div class="list-group">
-    <a href="#" class="list-group-item active">
-        <h4 class="list-group-item-heading">
-            资料分类
-        </h4>
-    </a>
-    <a href="#" class="list-group-item">
-        <h4 class="list-group-item-heading">
-            <button @click="listType = 'BillList'">订单管理</button>
-        </h4>
-    </a>
-    <a href="#" class="list-group-item">
-        <h4 class="list-group-item-heading">
-            <button @click="listType = 'CarList'">车辆管理</button>
-        </h4>
-    </a>
-</div>
-        </div>
-        
-        <div class="col-sm-10 col-md-10 col-lg-10 pull-right" style="top:60px;">
-          <ul id="myTab" class="nav nav-tabs">
-                <li class="active">
-                    <a href="#home" data-toggle="tab">
-                        商业险
+    <div class="">
+        <div class="row" style="margin:0px 10px 10px 0px;">
+            <div
+                class="col-sm-2 col-md-2 col-lg-2"
+                style="padding:0px 10px 0px 0px;margin:0px 20px 0px 0px;z-index:9999;position:fixed;top:70px;left:15px;">
+                <div class="list-group">
+                    <a href="#" class="list-group-item active">
+                        <h4 class="list-group-item-heading">
+                            资料分类
+                        </h4>
                     </a>
-                </li>
-                <li>
-                    <a href="#CLI" data-toggle="tab">强制险</a>
-                </li>
-                <li>
-                    <a href="#GAP" data-toggle="tab">验车</a>
-                </li>
-                
-                    <form class="navbar-form navbar-right" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search"></div>
-                            <button type="submit" class="btn btn-default">提交</button>
-                        </form>
-                    
-                </ul>
-                <div id="myTabContent" class="tab-content">
-                    <div class="tab-pane fade in active" id="home">
-                        <component :is="listType"></component>
-                        
-                    </div>
-                    <div class="tab-pane fade" id="CLI">
-                        <component :is="listType"></component>
-                       
-                    </div>
-                    <div class="tab-pane fade" id="GAP">
-                        <p>jMeter 是一款开源的测试软件。它是 100% 纯 Java 应用程序，用于负载和性能测试。</p>
-                    </div>
+                    <a href="#" class="list-group-item">
+                        <h4 class="list-group-item-heading">
+                            <button @click="listType = 'BillList'">订单管理</button>
+                        </h4>
+                    </a>
+                    <a href="#" class="list-group-item">
+                        <h4 class="list-group-item-heading">
+                            <button @click="listType = 'CarList'">车辆管理</button>
+                        </h4>
+                    </a>
                 </div>
+            </div>
+
+            <div class="col-sm-10 col-md-10 col-lg-10 pull-right" style="top:60px;">
+
+                <!-- <ul id="myTab" class="nav nav-tabs"> <li class="active"> <a href="#home"
+                data-toggle="tab"> 商业险 </a> </li> <li> <a href="#CLI" data-toggle="tab">强制险</a>
+                </li> <li> <a href="#GAP" data-toggle="tab">验车</a> </li> <form
+                class="navbar-form navbar-right" role="search"> <div class="form-group"> <input
+                type="text" class="form-control" placeholder="Search"></div> <button
+                type="submit" class="btn btn-default">提交</button> </form> </ul> <div
+                id="myTabContent" class="tab-content"> <div class="tab-pane fade in active"
+                id="home"> <component :is="listType"></component> </div> <div class="tab-pane
+                fade" id="CLI"> <component :is="listType"></component> </div> <div
+                class="tab-pane fade" id="GAP"> <p>jMeter 是一款开源的测试软件。它是 100% 纯 Java
+                应用程序，用于负载和性能测试。</p> </div> </div> -->
+
+                <div class="checkbox pull-left" style="margin-left:10px;">
+                    <label style="margin-left:10px">
+                        <input type="checkbox" > <span style="font-size:14px;">交强险</span>
+                        </label>
+                        <label style="margin-left:10px">
+                        <input type="checkbox" > <span style="font-size:14px;">商业险
+                        </span>                         </label>
+                        <label style="margin-left:10px">
+                        <input type="checkbox" > <span style="font-size:14px;">验车
+                        </span>                         </label>
+                        <label style="margin-left:10px">
+                        <input type="checkbox" > <span style="font-size:14px;">近一周
+                        </span>                         </label>
+                        <label style="margin-left:10px">
+                        <input type="checkbox" > <span style="font-size:14px;">近一月
+                        </span>                         </label>
+                        <label style="margin-left:10px">
+                        <input type="checkbox" > <span style="font-size:14px;">近三月
+                        </span>                         </label>
+                    </div>
+                    <div class= "pull-left" style="background-color:red;">kasjfhkafjhkajsf</div>
+                    <CarList style="margin-top:40px;"></CarList>
+                </div>
+                
+                 
+            </div>
         </div>
-    </div>
-</div>
-</template>
-<script>
+    </template>
+    <script>
 
-import BillList from './BillList'
-import CarList from './CarList'
+        import BillList from './BillList'
+        import CarList from './CarList'
 
-export default {
-  name: 'Manager',
-  components: {
-    BillList,
-    CarList
-  },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      listType:'BillList'
-    }
-  }
-}
-</script>
+        export default {
+            name: 'Manager',
+            components: {
+                BillList,
+                CarList
+            },
+            data() {
+                return {msg: 'Welcome to Your Vue.js App', listType: 'BillList'}
+            }
+        }
+    </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    <!-- Add "scoped" attribute to limit CSS to this component only -->
+    <style scoped="scoped">
+        h1,
+        h2 {
+            font-weight: normal;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            display: inline-block;
+            margin: 0 10px;
+        }
+        a {
+            color: #42b983;
+        }
+    </style>
